69a89e72aaca8e830cd9aa8ef89d7855b4aa115c
Daniel Portela Byrne <daniel.portelabyrne@ucdconnect.ie>
2020-02-23 21:14:20 +0000

    testCheckInstersection implemented and corrected.

--- src/Testing/BoardTest.java
+++ src/Testing/BoardTest.java
@@ -99,31 +99,18 @@ class BoardTest extends Board
     void testCheckIntersection()
     {
     	board.placeWord(3, 7, "HELLO", frame, true);
-    	//board.placeWord(2, 6, "HEY", frame, false);
-
-        System.out.println(board);
 
     	Tile[] E = {new Tile('E', 1), null ,null};
     	
     	assertEquals(Arrays.toString(E), Arrays.toString(board.checkIntersection(4, 6, "HEY", false)));
-    	
-    	board.resetBoard();
-    	
-    	board.placeWord(5, 7, "HAIRY", frame, true);
-    	//board.placeWord(4, 6, "MARRY", frame, false);
 
-    	Tile[] A = {new Tile('A', 1), null, null, null, null};
-    	
-    	assertEquals(Arrays.toString(A), Arrays.toString(board.checkIntersection(2, 6, "MARRY", false)));
-    	
-    	board.resetBoard();
-    	
-    	board.placeWord(2, 9, "BINGO", frame, true);
-    	//board.placeWord(1, 8, "LITTLE", frame, false);
+    	Tile[] L = {new Tile('L', 1), null, null, null, null};
 
-        Tile[] I = {new Tile('I', 1), null, null, null, null, null};
+    	assertEquals(Arrays.toString(L), Arrays.toString(board.checkIntersection(5, 7, "LEMON", false)));
+
+        Tile[] O = {new Tile('O', 1), null, null, null};
     	
-    	assertEquals(Arrays.toString(I), Arrays.toString(board.checkIntersection(2, 6, "LITTLE", false)));
+    	assertEquals(Arrays.toString(O), Arrays.toString(board.checkIntersection(7, 5, "GOLD", false)));
     	 
     	board.resetBoard();
     }
