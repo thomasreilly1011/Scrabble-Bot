220dbf8de428398eaeb1c06ac26ab575025e2ba8
Daniel Portela Byrne <daniel.portelabyrne@ucdconnect.ie>
2020-02-23 12:07:47 +0000

    Board is printing out correctly with new row and column borders added.

--- src/main/Board.java
+++ src/main/Board.java
@@ -259,6 +259,25 @@ public class Board
             //Now check that the frame has all remaining required letters to finish making up the word.
         return frame.hasString(word);
     }
+    
+    
+    void testResetBoard()
+    {
+    	Board board = new Board();
+    	
+    	Pool.set();
+
+        Frame frame = new Frame();
+
+        frame.createTestableFrame();
+
+        System.out.println(frame);
+
+        System.out.println(placeWord(3, 7, "HELLO", frame, true));
+        
+        System.out.println(board.toString());
+
+    }
 
 
     @Override
@@ -309,7 +328,5 @@ public class Board
         board.append(" ----------------------------------------------------------------------------\n");
         return board.toString();
     }
-
-
 
 }
