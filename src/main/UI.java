e4260181e1f65f187ee3a095f3df732a42a0801b
sjlacey <seanjlacey@outlook.com>
2020-02-28 13:51:33 +0000

    Fleshed out the scrabble and UI classes in collaboration with Thomas.

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,13 +1,31 @@
 package main;
 
+import java.util.Scanner;
+
 public class UI
 {
     //this exists
     //TODO check scrabble class
 
-    public void playersmove()
+    private static Scanner in = new Scanner(System.in);
+
+    public static Player playerInit()
     {
-        //stuff
+
+        System.out.println("Enter a player's name:");
+        String s = in.nextLine();
+
+        Player player = new Player(s);
+
+        System.out.println("\n" + s + "'s Frame:\n" + player.getFrame() + "\n");
+
+        return player;
     }
 
+    public static void playerMove(Player player)
+    {
+        System.out.println(player.getPlayerName() + "'s move. Enter the co-ords (bottom left being 1,1) you would like to place the first letter of your first word in:\n");
+        String s = in.nextLine();
+
+    }
 }
