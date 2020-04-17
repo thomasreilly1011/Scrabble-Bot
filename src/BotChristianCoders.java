33832cc38c760491952e84bee0eede994a039f32
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-04-17 16:01:22 +0100

    Added descriptions of what each of the skeleton functions should do.

--- src/BotChristianCoders.java
+++ src/BotChristianCoders.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 
 public class BotChristianCoders implements BotAPI {
+    private PlayerAPI me;
+    private OpponentAPI opponent;
+    private BoardAPI board;
+    private UserInterfaceAPI info;
+    private DictionaryAPI dictionary;
+
+    public BotChristianCoders(Player me, Player opponent, Board board, UserInterface ui, Dictionary dictionary) {
+        this.me = me;
+        this.opponent = opponent;
+        this.board = board;
+        this.info = ui;
+        this.dictionary = dictionary;
+    }
 
     @Override
     public String getCommand() {
@@ -23,9 +36,10 @@ public class BotChristianCoders implements BotAPI {
     /**
      * 1. Search the board for all possible word places soring them in a list in the form H8 A L****** where *'s
      * represent possible letters.
-     * @return ArrayList<String> of all possible word placements.
+     * @return ArrayList<String> of all possible word placements int the form H8 A L****** where *'s represent possible letters.
      */
-    private ArrayList<String> findPossibleWords() {
+    private ArrayList<String> findPossibleWords()
+    {
         return null;
     }
 
@@ -33,19 +47,19 @@ public class BotChristianCoders implements BotAPI {
      * 2. Search through the dictionary word tree using the letters found and replacing the *'s with letter permutations
      * from the frame.
      * @param possibleWords ArrayList<String> of possible word placements in the form H8 A L******
-     * @return ArrayList<String> of all legal word word placements in the form H8 A legalword, where legalword is a fully
+     * @return ArrayList<Word> of all legal word word objects.
      * defined, legal word.
      */
-    private ArrayList<String> findLegalWords(ArrayList<String> possibleWords) {
+    private ArrayList<Word> findLegalWords(ArrayList<String> possibleWords) {
         return null;
     }
 
     /**
      * 3. Score the word Produced.
-     * @param legalWords ArrayList<String> of legal word placements in the form H8 A legalword
-     * @return A single legal word placement that is the highest scoring option.
+     * @param legalWords ArrayList<Word> of legal Word objects.
+     * @return A single legal Word object that is the highest scoring option.
      */
-    private String mostValuableWord(ArrayList<String> legalWords) {
+    private Word mostValuableWord(ArrayList<Word> legalWords) {
         //TODO, seems relatively simple, arraylist of the words is given, method must calculate all their scores and play the highest one. Need to delve into his code to figure out how scoring works.
         return null;
     }
