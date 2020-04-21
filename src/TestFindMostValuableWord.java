ed3f250232d69c39f2a4cf3c54b82b4ffdf3866b
Daniel Byrne <daniel.portelabyrne@ucdconnect.ie>
2020-04-21 16:03:41 +0100

    Tester class for finding most valuable word almost finished. As of now the function is calculating scores for each word well and returning the word with the highest score. Only need to make sure it's utilising the correct multipliers.

--- src/TestFindMostValuableWord.java
+++ src/TestFindMostValuableWord.java
@@ -12,54 +12,25 @@ public class TestFindMostValuableWord {
         testWords.add(new Word(7, 7, true, "Beans"));
         testWords.add(new Word(7, 3, true, "Xylophone"));
 
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-//        board.getSquare(7, 7).add(new Tile('H'));
-
-
         Word mvw = test.mostValuableWord(testWords);
 
-        System.out.println(mvw.toString());
+        System.out.println("\nMost valuable word to place is '"+mvw.toString()+"'");
     }
 
     private ArrayList<Coordinates> newLetterCoords;
+    private static final int[] TILE_VALUE = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
     private int getWordPoints(Word word)
     {
-
-        int wordValue = 0;
-        int wordMultiplier = 1;
+        // place precondition: isLegal is true
+        newLetterCoords = new ArrayList<>();
         int r = word.getFirstRow();
         int c = word.getFirstColumn();
         for (int i = 0; i<word.length(); i++)
         {
-            int letterValue = board.getSquareCopy(r, c).getTile().getValue();
-            if (newLetterCoords.contains(new Coordinates(r,c)))
+            if (!board.getSquareCopy(r,c).isOccupied())
             {
-                wordValue = wordValue + letterValue * board.getSquareCopy(r, c).getLetterMuliplier();
-                wordMultiplier = wordMultiplier * board.getSquareCopy(r, c).getWordMultiplier();
-            }
-            else
-            {
-                wordValue = wordValue + letterValue;
+                newLetterCoords.add(new Coordinates(r,c));
             }
             if (word.isHorizontal())
             {
@@ -70,7 +41,45 @@ public class TestFindMostValuableWord {
                 r++;
             }
         }
-        return wordValue * wordMultiplier;
+        int score = 0;
+        int wordValue = 0;
+        int wordMultiplier = 1;
+
+        System.out.println("\nWord is: " + word.toString());
+        System.out.println("Length of word is " + word.length());
+        for (int i = 0; i<word.length(); i++)
+        {
+            char letter = word.getLetter(i);
+            letter = Character.toUpperCase(letter);
+            System.out.println("\nLetter at position [" + i + "] of " + word.toString() + " is '" + letter + "'");
+
+            int letterValue = TILE_VALUE[(int) letter - (int) 'A'];
+            System.out.println("Value of letter at position " + i + " of " + word.toString() + " is '" + letterValue + "'\n");
+
+            if (newLetterCoords.contains(new Coordinates(r,c)))
+            {
+                wordValue = wordValue + letterValue * board.getSquareCopy(r, c).getLetterMuliplier();
+                wordMultiplier = wordMultiplier * board.getSquareCopy(r, c).getWordMultiplier();
+                System.out.println("WordValue at iteration " + i + " is: " + wordValue);
+                System.out.println("WordMultiplier value at iteration " + i + " is: " + wordMultiplier);
+            }
+            else
+            {
+                wordValue = wordValue + letterValue;
+                System.out.println("WordValue at iteration " + i + " is: " + wordValue);
+            }
+            if (word.isHorizontal())
+            {
+                c++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        score = wordValue * wordMultiplier;
+        System.out.println("Score for word '"+word.toString()+"' is : "+score);
+        return score;
     }
 
 
